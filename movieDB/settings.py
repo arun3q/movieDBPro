162fd3d352c00cd939ad994410ee37e23ebcb1c8
arun3q <arun3q@gmail.com>
2018-10-15 16:51:20 +0000

    changed setting.py for whitenoise

--- movieDB/settings.py
+++ movieDB/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# whitenoise.storage.CompressedManifestStaticFilesStorage
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
